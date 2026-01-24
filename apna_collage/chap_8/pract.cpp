db2d7a18f14a5aeeee95ca4900f33f2b83259ea2
priyansh sahu <sahupriyansh298@gmail.com>
2026-01-24 23:22:31 +0530

    .

--- apna_collage/chap_8/pract.cpp
+++ apna_collage/chap_8/pract.cpp
@@ -1,16 +1,77 @@
 #include<iostream>
+#include<climits>
 using namespace std;
+
+int  LinearSearch(int arr[], int size, int target) {
+    for(int i=0; i<size; i++) {
+        if(arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int reverseArry(int arr[], int n) {
+    int st = 0, end = n-1;
+    while(st < end) {
+        swap(arr[st], arr[end]);
+        st++;
+        end--;  
+    } 
+}
+
+int smallestNumber(int nums[], int n) {
+
+    int smallest = INT_MAX;
+    for(int i=0; i<n; i++) {
+        if(nums[i] < smallest) {
+            smallest = nums[i];
+        }
+    }
+     return smallest;
+}
+
+int LargestNumber(int nums[], int n) {
+    int largest = INT_MIN;
+    for(int i=0; i<n; i++) {
+        if(nums[i] > largest) {
+            largest= nums[i];
+        }
+    }
+    return largest;
+}
+
+int secondLargestNumber(int nums[], int n) {
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for(int i=0; i<n; i++) {
+        if(nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+        }
+        else if(nums[i] > secondLargest && nums[i] != largest) {
+            secondLargest = nums[i];
+    }
+    return secondLargest;
+}
+}
 
 
 int main() {
-    int marks[9];
-    int size = 9;
+    int arr[] = {4, 2, 7, 8, 1, 2, 6};
+    int size = 7, target = 2;
 
-    for(int i=0; i<size; i++) {
-        cin >> marks[i];
-    }
+    cout << "LinearSearch: " << LinearSearch(arr, size, target) << endl;
 
+    reverseArry(arr, size); 
     for(int i=0; i<size; i++) {
-        cout << "Marks of student " << marks[i] << endl;
-    }
+        cout << arr[i] << " ";
+    };
+
+    int nums[] = {5, 15, 22, 1, -15, 24, 90};
+    int n = 7;
+    cout << "\nSmallest Number: " << smallestNumber(nums, n) << endl;
+    cout << "Largest Number: " << LargestNumber(nums, n) << endl;
+    
 }
